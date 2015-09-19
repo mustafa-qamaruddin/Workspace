d6682ffb1cbfd64c699526faf4b844836e3a2708
Mustafa Qamar-ud-Din <m.qamaruddin@nilecode.com>
2015-09-19 23:31:45 +0300

    chocolate feast

--- chocolate-feast/main.cpp
+++ chocolate-feast/main.cpp
@@ -8,19 +8,23 @@ using namespace std;
 
 typedef unsigned long long ll;
 
-ll chocolate_feast(ll n, ll c, ll m){
-    ll number_of_chocos = n / c;
-    ll remainder_wrappers = number_of_chocos % m;
-    ll additional_chocos = number_of_chocos / m;
-    ll extra_chocos = (remainder_wrappers + additional_chocos) / m;
-    return (number_of_chocos + additional_chocos + extra_chocos);
+ll chocolate_feast(ll n, ll c, ll m)
+{
+    ll n_of_chocos = n / c;
+    ll ret = n_of_chocos;
+    while(n_of_chocos / m > 0)
+    {
+        ret += n_of_chocos / m;
+        n_of_chocos = n_of_chocos % m + n_of_chocos / m;
+    }
+    return ret;
 }
 
 int main()
 {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    freopen("inputs.dat", "r", stdin);
-    freopen("my_outputs.dat", "w", stdout);
+//    freopen("inputs.dat", "r", stdin);
+//    freopen("my_outputs.dat", "w", stdout);
     ll t,n,c,m;
     cin>>t;
     while(t--)
