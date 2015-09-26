dbb3a59fdb7b54fce6c34d10189935b1b894745b
Mustafa Qamar-ud-Din <m.qamaruddin@nilecode.com>
2015-09-26 05:37:46 +0200

    bfs-reach

---
+++ bfs-short-reach/main.cpp
@@ -0,0 +1,113 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <list>
+
+using namespace std;
+
+#define Edge_Cost 6
+
+enum COLORS {WHITE, GREY, BLACK};
+
+struct node
+{
+    COLORS color = WHITE;
+    bool visited = false;
+    vector<int> adjl;
+    int parent;
+    int distance = -1;
+};
+
+void display_graph(node* a_graph, int sz)
+{
+    for(int i = 0; i <= sz; i++)
+    {
+        cout << i << ":";
+        vector<int>::iterator itr = a_graph[i].adjl.begin();
+        while(itr != a_graph[i].adjl.end())
+        {
+            cout << *itr << ", ";
+            ++itr;
+        }
+        cout << endl;
+    }
+}
+
+
+void print_distances(node* a_graph, int sz, int s)
+{
+    for(int i = 1; i <= sz; i++)
+    {
+        if(i != s)
+            cout << a_graph[i].distance << " ";
+    }
+    cout << endl;
+}
+
+void calculate_distance(node* a_graph, int an_index){
+    int d = 0;
+    int i = an_index;
+    while(a_graph[i].parent != NULL){
+        d += Edge_Cost;
+        i = a_graph[i].parent;
+    }
+    a_graph[an_index].distance = d;
+}
+
+void bfs(node* a_graph, int sz, int s)
+{
+    queue<int> q;
+    a_graph[s].parent = NULL;
+    a_graph[s].color = GREY;
+    q.push(s);
+    int distance = 0;
+    while(!q.empty())
+    {
+        int k = q.front();
+        q.pop();
+        a_graph[k].visited = true;
+        vector<int>::iterator itr = a_graph[k].adjl.begin();
+        vector<int>::iterator itr_end = a_graph[k].adjl.end();
+        while(itr != itr_end)
+        {
+            if(a_graph[*itr].visited)
+            {
+                ++itr;
+                continue;
+            }
+            a_graph[*itr].color = GREY;
+            a_graph[*itr].parent = k;
+            q.push(*itr);
+            calculate_distance(a_graph, *itr);
+            ++itr;
+        }
+    }
+    print_distances(a_graph, sz, s);
+}
+
+int main()
+{
+//    freopen("input01.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int t, n, m, x, y, s;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n >> m;
+        node* my_graph = new node[n+1];
+        for(int i = 0; i < m; i++)
+        {
+            cin >> x >> y;
+            my_graph[x].adjl.push_back(y);
+            my_graph[y].adjl.push_back(x);
+        }
+        cin >> s;
+        bfs(my_graph, n, s);
+    }
+    return 0;
+}
